007b42d89a37805e7681ac0af8457518ddafee6a
Quimby <laura_j_quimby@hotmail.com>
2016-03-18 14:47:27 -0400

    Tried Bootstrap modals, put base code in place.

--- DotNetLimber/src/DotNetLimber/wwwroot/js/site.js
+++ DotNetLimber/src/DotNetLimber/wwwroot/js/site.js
@@ -1,29 +1,3 @@
-﻿// Write your Javascript code.
-var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
-var $caption = $("<p></p>");
-
-//An image to overlay
-$overlay.append($image);
-
-//Add overlay
-$("body").append($overlay);
-
-//Capture the click event on a link to an image
-$("#gallery a").click(function (event) {
-    event.preventDefault();
-    var imageLocation = $(this).attr("href");
-    //Update the overlay with the image linked in the link
-    $image.attr("src", imageLocation)
-
-    //Show the overlay
-    $overlay.show();
-
-});
-
-//When overlay is clicked
-$overlay.click(function () {
-    //Hide the overlay
-    $overlay.hide();
-
-});
+﻿$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+})
