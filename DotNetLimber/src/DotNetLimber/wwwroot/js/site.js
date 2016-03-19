ff22b36b44e4920eb0a265523eaa548156e19805
Quimby <laura_j_quimby@hotmail.com>
2016-03-19 11:39:20 -0400

    Added javascript for modals for all gear items.

--- DotNetLimber/src/DotNetLimber/wwwroot/js/site.js
+++ DotNetLimber/src/DotNetLimber/wwwroot/js/site.js
@@ -1,3 +1,15 @@
 ﻿$('#myModal').on('shown.bs.modal', function () {
     $('#myInput').focus()
 })
+
+$('#myModal1').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+})
+
+$('#myModal2').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+})
+
+$('#myModal3').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+})
